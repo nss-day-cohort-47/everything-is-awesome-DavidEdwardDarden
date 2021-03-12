620e931ce12ff57c6cb74a02f03181de117f0b13
David Darden <davidedarden83@gmail.com>
2021-03-12 13:05:40 -0600

    completed mvp

--- scripts/legos/LegoDetail.js
+++ scripts/legos/LegoDetail.js
@@ -2,8 +2,14 @@ import { invertColor } from './../helpers.js';
 
 export const LegoDetail = (brick) => {
 
+  const makeslegoUpperCase = (nameses) => {
+    return nameses.toUpperCase()
+  }
+  
+
   let block = `<section class="block-wrapper" style="background-color:#${brick.ColorHex}">
-              <h3>Name: ${brick.LegoName}</h3>
+              <h3>Name: ${makeslegoUpperCase(brick.LegoName)}</h3>
+              <div class="block-notes">Notes: ${brick.Notes}</div>
               <div class="block-years">Manufactured ${brick.YearFrom} - ${brick.YearTo}</div>
               `;
   const link = brick.ColorstreamLinkImage;
--- scripts/main.js
+++ scripts/main.js
@@ -5,16 +5,39 @@ import { makeLegoList } from './legos/LegoList.js';
 const navElement = document.querySelector("nav");
 
 navElement.addEventListener("click", (event) => {
-	if (event.target.id === "showBlue") {
-		filterLegos("Blue")
+	if (event.target.id === "showRed") {
+		filterLegos("Red")
 	} else if (event.target.id === "showAll") {
 		makeLegoList(useLegos())
 	}
 })
 
+navElement.addEventListener("click", (event) => {
+	if (event.target.id === "showGreen") {
+		filterLegos("Green")
+	} else if (event.target.id === "showAll") {
+		makeLegoList(useLegos())
+	}
+})
+
+
+
+//dropdown filter for material 
+const materialElement = document.querySelector("#materialSelector")
+materialElement.addEventListener("change", (event) => {
+	if(event.target.id === "materialSelector") {
+		const materialValue = (event.target.value);
+		filterLegos(materialValue);
+	}else if (event.target.id === "showAll"){
+		makesLegoList(useLegos())
+	}
+})
+
+
+//filter mater
 const filterLegos = (whatFilter) => {
 	const filterArray = useLegos().filter(singleLego => {
-		if (singleLego.LegoName.includes(whatFilter)) {
+		if (singleLego.Material.includes(whatFilter)) {
 			return singleLego;
 		}
 	})
